419bc413dc98f0f0aeeacb38eced13c19f2a9163
Diana Veselkina <vess_di@mail.ru>
2025-06-21 18:24:10 +0300

    creating a project

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainPage } from './main-page/main-page';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MainPage],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'ToDo';
+}
